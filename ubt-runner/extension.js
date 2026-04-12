2d529547f023258eecab4f38750d0d569be3b143
Cyrus-13 <yassawar4@gmail.com>
2026-04-12 13:27:45 +0100

    picking filles

--- ubt-runner/extension.js
+++ ubt-runner/extension.js
@@ -1,27 +1,62 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// hada hir vscode lib mstoriya f const variabl
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
+const path = require('path');
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ubt-runner" is now active!');
 
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ubt-runner.helloWorld', function () {
+	const disposable = vscode.commands.registerCommand('ubt-runner.RunUBT', async function () {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from UBT-Runner!');
+		//wlkin 9bal mt ruunna lcommand hy5ssni n geti user UBT location
+		//(hnst3ml user input f bdya wlkin n9d nrdha ka dedicta automatcly mnb3d)
+		const userPaths = await getFiles();
+		if (userPaths){
+				const ubt = userPaths.ubtLocation;
+				const uproject = userPaths.projectLocation;
+				const projectName = path.basename(uproject,'.uproject')
+				const platform = "Win64"
+				const configuration = "Development"
+				const finalCommand = `"${ubt}" ${projectName} ${platform} ${configuration} "${uproject}"`;
+				vscode.window.showInformationMessage(`hahiya command dyalk a m3lam ${finalCommand}`);
+			}
+
+		async function getFiles() {
+			const ubtUri =await vscode.window.showOpenDialog({
+				canSelectMany: false, // We only want them to pick one file
+                openLabel: 'Select Build.bat',
+                title: 'Step 1/2: Select UBT Batch File',
+			
+				});
+					
+			if (!ubtUri || !ubtUri[0]) {
+                return undefined;
+            }
+			const ubtResult = ubtUri[0].fsPath;
+
+
+    		const projectUri = await vscode.window.showOpenDialog({
+        		canSelectMany: false,
+        		openLabel: 'Select .uproject file',
+				title: 'step 2/2: Select your Project file',
+        		Filters: {'Unreal Projects' : ['uproject']}
+    		});
+    
+    		if (!projectUri || !projectUri[0]){
+				return undefined;
+			} 
+			const projectResult = projectUri[0].fsPath;
+			return{
+				ubtLocation: ubtResult,
+				projectLocation: projectResult
+			}
+ 
+		}
+
+		
 	});
 
 	context.subscriptions.push(disposable);
